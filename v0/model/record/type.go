a7e61e9915ddb719fee8381f4fc71b580ab9314d
Elizabeth Harper <epharper@upenn.edu>
2020-04-27 15:59:50 -0400

    Added missing 'searches' field on record type struct

--- v0/model/record/type.go
+++ v0/model/record/type.go
@@ -4,27 +4,29 @@ import (
 	"github.com/VEuPathDB/lib-go-wdk-api/v0/model/answer"
 	"github.com/VEuPathDB/lib-go-wdk-api/v0/model/attribute"
 	"github.com/VEuPathDB/lib-go-wdk-api/v0/model/common"
+	"github.com/VEuPathDB/lib-go-wdk-api/v0/model/search"
 	"github.com/VEuPathDB/lib-go-wdk-api/v0/optional"
 )
 
 type Type struct {
-	FullName                     string            `json:"fullName"`
-	DisplayName                  optional.String   `json:"displayName"`
-	DisplayNamePlural            optional.String   `json:"displayNamePlural"`
-	ShortDisplayName             optional.String   `json:"shortDisplayName"`
-	ShortDisplayNamePlural       optional.String   `json:"shortDisplayNamePlural"`
-	NativeDisplayName            optional.String   `json:"nativeDisplayName"`
-	NativeDisplayNamePlural      optional.String   `json:"nativeDisplayNamePlural"`
-	NativeShortDisplayName       optional.String   `json:"nativeShortDisplayName"`
-	NativeShortDisplayNamePlural optional.String   `json:"nativeShortDisplayNamePlural"`
-	UrlSegment                   string            `json:"urlSegment"`
-	IconName                     optional.String   `json:"iconName"`
-	UseBasket                    bool              `json:"useBasket"`
-	Description                  optional.String   `json:"description"`
-	Properties                   common.Properties `json:"properties"`
-	Formats                      []answer.Format   `json:"formats"`
-	HasAllRecordsQuery           bool              `json:"hasAllRecordsQuery"`
-	PrimaryKeyRefs               []string          `json:"primaryKeyColumnRefs"`
-	Attributes                   []attribute.Field `json:"attributes"`
-	Tables                       []Table           `json:"tables"`
+	FullName                     string               `json:"fullName"`
+	DisplayName                  optional.String      `json:"displayName"`
+	DisplayNamePlural            optional.String      `json:"displayNamePlural"`
+	ShortDisplayName             optional.String      `json:"shortDisplayName"`
+	ShortDisplayNamePlural       optional.String      `json:"shortDisplayNamePlural"`
+	NativeDisplayName            optional.String      `json:"nativeDisplayName"`
+	NativeDisplayNamePlural      optional.String      `json:"nativeDisplayNamePlural"`
+	NativeShortDisplayName       optional.String      `json:"nativeShortDisplayName"`
+	NativeShortDisplayNamePlural optional.String      `json:"nativeShortDisplayNamePlural"`
+	UrlSegment                   string               `json:"urlSegment"`
+	IconName                     optional.String      `json:"iconName"`
+	UseBasket                    bool                 `json:"useBasket"`
+	Description                  optional.String      `json:"description"`
+	Properties                   common.Properties    `json:"properties"`
+	Formats                      []answer.Format      `json:"formats"`
+	HasAllRecordsQuery           bool                 `json:"hasAllRecordsQuery"`
+	PrimaryKeyRefs               []string             `json:"primaryKeyColumnRefs"`
+	Attributes                   []attribute.Field    `json:"attributes"`
+	Tables                       []Table              `json:"tables"`
+	Searches                     []search.ShortSearch `json:"searches"`
 }
